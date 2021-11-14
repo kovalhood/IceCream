8d8e851e500b9a772f3dd83be7134fba44cfa8d6
Valentyn Kovalchuk <kovalhood@gmail.com>
2021-11-14 13:11:34 +0200

    Reviews-6

--- src/js/slick-script.js
+++ src/js/slick-script.js
@@ -1,3 +1,13 @@
 jQuery(function () {
-    $('.reviews').slick();
+    $('.reviews').slick({
+        arrows: false,
+        dots: true,
+        speed: 1000,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 10000,
+        pauseOnFocus:true,
+        pauseOnHover: true,
+        pauseOnDotsHover:true,
+    });
 });
