c12093a644b3b0d366cd01ff4c12724363653c55
jsevruk <j.sevruk@gmail.com>
2021-11-15 19:26:03 +0200

    Додала вибір продукції + мілкі коригування

--- src/js/modal.js
+++ src/js/modal.js
@@ -31,17 +31,17 @@
 })();
 
 
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-franchise-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
-//     modal: document.querySelector('[data-modal-franchise]'),
-//   };
-//
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-//
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
-// })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-franchise-open]'),
+    closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
+    modal: document.querySelector('[data-modal-franchise]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
