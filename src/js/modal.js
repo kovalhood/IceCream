3b6c89491eef7891d77c408786162d82bb848b20
Valentyn Kovalchuk <kovalhood@gmail.com>
2021-11-15 19:41:47 +0200

    Layout-20

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,36 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-locations-open]'),
+    closeModalBtn: document.querySelector('[data-modal-locations-close]'),
+    modal: document.querySelector('[data-modal-locations]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-franchise-open]'),
+    closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
+    modal: document.querySelector('[data-modal-franchise]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
