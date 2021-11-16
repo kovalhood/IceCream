2fd04e55fe98b6a1f2854dfc490f470f910d085e
Valentyn Kovalchuk <kovalhood@gmail.com>
2021-11-16 22:02:23 +0200

    Layout-34

--- src/js/modal.js
+++ src/js/modal.js
@@ -45,3 +45,16 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-menu]'),
+    closeModalBtn: document.querySelector('[data-modal-close-menu]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
