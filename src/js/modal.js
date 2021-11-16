783bcacbc29832dfa0778e6dbaf86af4f3df7525
jsevruk <j.sevruk@gmail.com>
2021-11-16 20:42:00 +0200

    скрипт для модалки мобільного меню

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,6 +13,18 @@
   }
 })();
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-menu]'),
+    closeModalBtn: document.querySelector('[data-modal-close-menu]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
 
 
 (() => {
