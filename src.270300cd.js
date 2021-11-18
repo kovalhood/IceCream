31ac2773178261cd014192784ed588d87c5da525
kovalhood <90193930+kovalhood@users.noreply.github.com>
2021-11-18 10:07:06 +0000

    Deploying to gh-pages from @ kovalhood/icecream@12880984e06c281ee372b95328fca7d54f664365 🚀

--- src.270300cd.js
+++ src.270300cd.js
@@ -124,4 +124,4 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
 require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/icecream/src.0d1fcd1b.js.map
+//# sourceMappingURL=/icecream/src.270300cd.js.map
